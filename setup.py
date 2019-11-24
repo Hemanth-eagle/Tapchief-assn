467ee710dbdc3d8434b69eb9418dd668df5f644f
Hemanth-eagle <43502615+Hemanth-eagle@users.noreply.github.com>
2019-11-24 23:51:53 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+       name=='Tap-search'
+       version='1.0'
+       description='Testing Texract application'
+       author='E.H.V.D.Prasad'
+       author_email="Eaglehemanth333@gmail.com"
+       packages=["Tap-search"]
+       install_requirements=['Texract']
